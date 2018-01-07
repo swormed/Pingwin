12a61d005e425e4f51c2e465f6f95f87eacb604f
swormed <sworm@vp.pl>
2018-01-07 23:10:27 +0100

    usuniecie assetow, dodanie nowych

---
+++ Assets/Scripts/garbage_collector.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class garbage_collector : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
