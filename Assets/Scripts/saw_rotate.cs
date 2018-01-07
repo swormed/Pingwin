6c1d183a35d0211b54f0e27fdd597a07457c371f
swormed <sworm@vp.pl>
2018-01-07 20:00:54 +0100

    Initial commit

---
+++ Assets/Scripts/saw_rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saw_rotate : MonoBehaviour {
+
+    public float rotateSpeed = 5.0f; //set it in the  inspector
+
+    void Update()
+    {
+        rotate();
+    }
+
+
+    void rotate()
+    {
+
+        transform.Rotate(Vector3.forward * rotateSpeed);
+    }
+}
